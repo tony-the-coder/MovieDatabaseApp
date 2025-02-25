39e4f6a4475605b18ef90ea01489c413b262a644
Tony Aldea <tonythecoder@outlook.com>
2025-02-25 15:17:44 -0500

    Update assembly informational version for BlazorWebAppMovies

--- BlazorWebAppMovies/obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ BlazorWebAppMovies/obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef272844473c484bd3fa2d461bb7980fc270c6ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b76285870ff525ac09706096d77268263e3d57")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
