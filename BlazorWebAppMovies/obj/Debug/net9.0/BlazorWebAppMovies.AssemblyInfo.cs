1e2c33c04660688680fba5d6d4743093f4379edc
Tony Aldea <tonythecoder@outlook.com>
2025-02-25 12:23:01 -0500

    Add initial database migration and update appsettings for connection string

--- BlazorWebAppMovies/obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ BlazorWebAppMovies/obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb857d96b6a0c189eacbf18a6f1c60ff5a2f1fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
