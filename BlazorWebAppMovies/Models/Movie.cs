99e818953ea4f14c58ad2983fac7cc855cd35b18
Tony Aldea <tonythecoder@outlook.com>
2025-02-25 13:05:20 -0500

    Refactor movie model and update database context with validation attributes

--- BlazorWebAppMovies/Models/Movie.cs
+++ BlazorWebAppMovies/Models/Movie.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorWebAppMovies.Models;
-
+// This page crates the database model, I believe
 public class Movie
 {
     // This is the primary key for the database
@@ -10,12 +10,18 @@ public class Movie
 
     // As a reminder, the "?" indicates that the propery can hold
     // a null value or the propery is nullable
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Title { get; set; }
 
     public DateOnly ReleaseDate { get; set; }
 
+    [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
     public string? Genre { get; set; }
 
+    [Range(0, 100)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
--- BlazorWebAppMovies/Program.cs
+++ BlazorWebAppMovies/Program.cs
@@ -16,7 +16,12 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
